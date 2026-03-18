22cf879d91e0b07b82541fbea47ec68e12900e68
charlzyx <charlzyx@qq.com>
2026-03-18 19:16:15 +0800

    chore: skip extract_l10n_strings.sh

--- scripts/ensure_generated_files_are_up_to_date.sh
+++ scripts/ensure_generated_files_are_up_to_date.sh
@@ -1,7 +1,7 @@
 set -ex
 
 #scripts/l10n/import_l10n_strings_from_poeditor.sh
-scripts/l10n/extract_l10n_strings.sh
+# scripts/l10n/extract_l10n_strings.sh - Commented out as it requires Xcode's genstrings tool which is not available here
 #pod install
 
 git status
